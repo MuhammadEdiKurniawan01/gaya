e5af3f9f0c48b02f8c009c760f723ee1c00703a9
MuhammadEdiKurniawan01 <edikurunyawan25@gmail.com>
2019-01-05 20:59:56 +0700

    projek 4

---
+++ rumus E=m.c.c.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int p,l,Energi;
+
+    cout<<"masukan masa : ";
+    cin>>p;
+    cout<<"masukan kecepatan cahaya : ";
+    cin>>l;
+    cout<<"           E = m x c x c \n";
+    Energi=p*l*l;
+    cout<<"energi yang didapat adalah :"<<Energi;
+    return 0;
+
+}
---
+++ rumus E=m.c.c.h
@@ -0,0 +1,16 @@
+#ifndef RUMUS E=M_C_C_H
+#define RUMUS E=M_C_C_H
+
+
+class rumus E=m.c.c
+{
+    public:
+        rumus E=m.c.c();
+        virtual ~rumus E=m.c.c();
+
+    protected:
+
+    private:
+};
+
+#endif // RUMUS E=M_C_C_H
